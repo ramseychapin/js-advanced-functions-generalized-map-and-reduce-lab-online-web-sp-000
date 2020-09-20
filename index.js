e882d493126da1b6ad52a647b6dd5b988ab8d63e
ramseychapin <ramsey@ramseychapin.com>
2020-09-19 20:49:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(arr, func) {
+	let newArray = [];
+	for (const element of arr) {
+		newArray.push(func(element))
+	}
+	return newArray;
+}
+
+function reduce(arr, func, start = 0) {
+	let total = start
+	for (let i = 0; i < arr.length; i++) {
+		if (isNaN(arr[i])) {
+			total = true
+		}
+		total = func(arr[i], total);
+	}
+	return total;
+}
